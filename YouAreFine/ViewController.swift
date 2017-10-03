d4915b500eb94f1f247e1aec3835bff56aac638c
msweet168 <mitchellsweet168@aol.com>
2017-10-02 20:27:04 -0400

    Initial Commit

---
+++ YouAreFine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YouAreFine
+//
+//  Created by Mitchell Sweet on 10/2/17.
+//  Copyright © 2017 Mitchell Sweet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
